152ddf70bb1c27900ef49a4cdb386104669791e1
nickledave <nicholdav@gmail.com>
2018-04-18 21:20:40 -0400

    fix bug in test/test_determine_symbols_and_mac_repeats.py

--- tests/test_determine_symbols_and_max_repeats.py
+++ tests/test_determine_symbols_and_max_repeats.py
@@ -2,15 +2,21 @@ from pomma.determine_symbols_and_max_repeats import determine_symbols_and_max_re
 
 # list of lists to test
 LOL = [
-    [1,1,2,3],
+    [1, 1, 2, 3],
     [1, 1, 1, 1, 2, 3],
     [1, 1, 2, 3],
+]
+
+LOL_mapped = [
+    [0, 0, 1, 2],
+    [0, 0, 0, 0, 1, 2],
+    [0, 0, 1, 2],
 ]
 
 
 def test_determine_symbols_and_max_repeats():
     symbols_and_max_repeats = determine_symbols_and_max_repeats(sequences=LOL)
     assert symbols_and_max_repeats['symbols'] == {1,2,3}
-    assert symbols_and_max_repeats['seqs_mapped'] == LOL
+    assert symbols_and_max_repeats['seqs_mapped'] == LOL_mapped
     assert symbols_and_max_repeats['max_repeats'] == {1:4, 2:1, 3:1}
     assert symbols_and_max_repeats['repeat_symbols'] == [1]
