c42c21e8d24bf353c073dd893344302f1530be19
nickledave <nicholdav@gmail.com>
2018-04-21 00:46:03 -0400

    add datasets import to pomma/__init__.py

--- pomma/__init__.py
+++ pomma/__init__.py
@@ -1,2 +1,2 @@
 from .pommafitter import POMMAfitter
-from . import first_order_markov, notmat_utils, plot_utils, statistics
+from . import first_order_markov, notmat_utils, plot_utils, statistics, datasets
